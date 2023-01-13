66d7d6f917ac90e930c0c1ad313271eb77880f5d
Zhogin Ivan <mr.i-zhogin@yandex.ru>
2023-01-13 14:46:25 +0300

    added few classes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,53 @@
+
+data class Post(
+    val id : Int,
+    val ownerId : Int,
+    val text : String,
+    val friendsOnly : Boolean,
+    val comment: comment,
+    val likes: likes,
+)
+
+
+//count (integer) — количество комментариев;
+//can_post* (boolean) — информация о том, может ли текущий пользователь комментировать запись;
+//groups_can_post (boolean) — информация о том, могут ли сообщества комментировать запись;
+//can_close(boolean) — может ли текущий пользователь закрыть комментарии к записи;
+//can_open(boolean) — может ли текущий пользователь открыть комментарии к записи.
+class comment(
+    count: Int = 0,
+    var canPost: Boolean = true,
+    var groupsCanPost: Boolean = true,
+    var canClose: Boolean = true,
+    var canOpen: Boolean = true,
+){
+    var counts = count
+        set(value){
+            if (value < 0)
+                return
+            field = value
+        }
+}
+
+//count (integer) — число пользователей, которым понравилась запись;
+//user_likes* (boolean) — наличие отметки «Мне нравится» от текущего пользователя;
+//can_like* (boolean) — информация о том, может ли текущий пользователь поставить отметку «Мне нравится»;
+//can_publish* (boolean) — информация о том, может ли текущий пользователь сделать репост записи.
+class likes(
+    count: Int = 0,
+    var userLikes: Boolean = true,
+    var canLike: Boolean = true,
+    var canPublish: Boolean = true
+){
+    var counts = count
+        set(value){
+            if (value < 0)
+                return
+            field = value
+        }
+}
+
+
 fun main() {
 
 }
